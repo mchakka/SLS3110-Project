993c70ff1629f14ed0a5c2f6b7ce87463cd38d73
Manas Chakka <mchakka@gmail.com>
2020-11-22 17:34:50 -0500

    hi

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import main
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/api/convert', methods=['GET', 'POST'])
+def convert():
+    actaulText = request.form['temp'] # text from a form submit
+    number = request.form['number']
+    file = main.function(actaulText, number) # Inan's work
+    #returns an mp3 file
+    return render_template("index.html", output=file)
 
 
 if __name__ == '__main__':
